052fa310b1d8828c2d6ebc3fdba53e47648529a1
Marco Luglio <marcodejulho@gmail.com>
2020-10-10 22:34:40 -0300

    Fix docker ip binding

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::net::TcpStream;
 
 fn main() {
 
-	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+	let listener = TcpListener::bind("0.0.0.0:80").unwrap();
 
 	for stream in listener.incoming() {
 		let stream = stream.unwrap();
